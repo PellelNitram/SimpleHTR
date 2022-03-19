e71eef66b1d94f9a34f5131e3882921998e7fc50
Martin L <konten.ma.le@gmail.com>
2022-03-19 17:36:07 +0100

    Add run_script bash script.

---
+++ run_script.sh
@@ -0,0 +1,8 @@
+cd src
+
+# python main.py --img_file ../data/word.png # Example 1
+# python main.py --img_file ../data/line.png # Example 2
+
+python main.py --img_file /home/martin/Development/WordDetectorNN/data/test/outdir/10.jpg
+
+# E.g. 'retro' is slightly miss classified; is that maybe because of a too small aabb box?
